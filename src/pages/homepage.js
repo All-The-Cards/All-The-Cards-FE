dbcf42c453b2cd401a9e3f4c4b216d162171e8b8
Tanner Hawkins <TTHawkins@student.fullsail.edu>
2022-08-29 14:50:41 -0400

    refactors homepage to function component

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,43 +1,39 @@
-import { React, Component } from 'react';
+import { React, useState } from 'react';
 import CardObject from '../components/CardObject/CardObject.js';
 import * as ServerTalk from '../functions/ServerTalk.js';
 
-export default class Home extends Component {
+const Home = (props) => {
 
-  state = {
+  const [state, setState] = useState({
     searchResults: [],
     resultsFound: "",
     serverResponse: "",
     searchInput: ""
-  }
+  })
 
-  constructor(props){
-    super(props)
-  }
-  
-  search(query){
+  const search = (query) => {
     //if query is empty, don't send
-    if (query.trim() == "/api/search/query=") {
-      return 
+    if (query.trim() === "/api/search/query=") {
+      return
     }
     //clear results
-    this.setState({
+    setState({
       searchResults: []
     })
 
     ServerTalk.post(query).then(response => {
-      if (response.length === 0){
-        this.setState({
+      if (response.length === 0) {
+        setState({
           resultsFound: <div>No Results Found</div>
         })
       }
       else {
-        this.setState({
+        setState({
           resultsFound: <div>{response.length} Results Found</div>,
-          searchResults: response.map((item, i) => <CardObject data={item} key={i}/>)
+          searchResults: response.map((item, i) => <CardObject data={item} key={i} />)
         })
       }
-      
+
       return response
     }).then(response => {
       console.log(response)
@@ -45,29 +41,28 @@ export default class Home extends Component {
 
   }
 
-  setInput(input){
-    this.setState({
+  const setInput = (input) => {
+    setState({
       searchInput: input.target.value
     })
   }
-  
-  render () {
-    return (
-      <div>
-        <input 
-          placeholder="Search..." 
-          value={this.state.searchInput}
-          onChange={(input) => this.setInput(input)}>
-        </input>
-        <button onClick={() => this.search("/api/search/query=" + this.state.searchInput)}>Search</button>
-        <div style={{width: '400px'}}>
-          <div>
-            {this.state.resultsFound}
-          </div>
-          {this.state.searchResults}
+  return (
+    <div>
+      <input
+        placeholder="Search..."
+        value={state.searchInput}
+        onChange={(input) => setInput(input)}>
+      </input>
+      <button onClick={() => search("/api/search/query=" + state.searchInput)}>Search</button>
+      <div style={{ width: '400px' }}>
+        <div>
+          {state.resultsFound}
         </div>
+        {state.searchResults}
       </div>
-    );
-  }
+    </div>
+  );
 
 };
+
+export default Home;
