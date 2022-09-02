1714e0575bf1ebe667145240478b3eed93e5a215
noahvstephenson <nvstephenson@student.fullsail.edu>
2022-09-02 18:44:02 -0400

    Deck loading from ID
    - Removed dummy decks from home page
    - Deck page now shows list of cards

--- src/pages/Deck.js
+++ src/pages/Deck.js
@@ -1,15 +1,65 @@
-import React from 'react'
+import { React, useState, useEffect } from 'react';
+import CardObject from '../components/CardObject/CardObject.js';
+import CardListObject from '../components/CardListObject/CardListObject.js';
+import * as server from '../functions/ServerTalk.js';
+import { useSearchParams, useNavigate } from 'react-router-dom';
 
-const Deck = () => {
+const Deck = (props) => {
+
+    const [state, setState] = useState({
+      data: []
+    })
+
+    const updateState = (objectToUpdate) => {
+        setState((previous) => ({
+          ...previous,
+          ...objectToUpdate
+        }))
+    }
+
+    const nav = useNavigate()
+    const id = useSearchParams()[0].toString()
+
+    useEffect(() => {
+      //clean up string from id format to search query format
+        getDeckById(id)
+    }, [id])
+
+    const getDeckById = (query) => {
+      query = "/api/get/deck/" + query
+      //if query is empty, don't send
+      if (query.trim() === "/api/get/deck/" ) {
+        return
+      }
+  
+      server.post(query).then(response => {
+        console.log(response)
+        //if invalid, just direct to search page
+        if (response.length === 0) {
+          nav('/search/')
+        }
+        else {
+          updateState({
+            data: response
+          })
+          console.log(response)
+        }
+  
+      })
+  
+    }
 
   return (
-
     <div>
-        Deck Page
+      <div>
+        {state.data.name}
+        {state.data.map((item, i) => 
+          <div style={{margin: '10px', display:'inline-block'}}key={i}><CardListObject data={item}/> <CardObject data={item}/></div>
+        )}
+      </div>
     </div>
+  );
 
+};
 
-  )
-}
-
-export default Deck
+export default Deck;
