13b05dc77696db553a111ca53fe9b5ed2fd201b6
Tanner Hawkins <TTHawkins@student.fullsail.edu>
2022-09-08 14:19:43 -0400

    deck style

--- src/components/CardStack/CardStack.js
+++ src/components/CardStack/CardStack.js
@@ -2,7 +2,7 @@ import { React } from "react";
 import CardObject from "../CardObject/CardObject";
 
 const CardStack = (props) => {
-    return (<div style={{display: 'flex', flexFlow: 'column'}}>
+    return (<div style={{display: 'flex', flexFlow: 'column', gap: "16px"}}>
         {props.label}
         {props.cards.map((card, index) => (
             <CardObject data={card} isCompact={props.isCompact}/>
