7f33f036bd20a5946603e4fdb6716c9f8a628d63
Tanner Hawkins <TTHawkins@student.fullsail.edu>
2022-10-26 14:28:06 -0400

    localstorag utils

--- src/functions/Utilities.js
+++ src/functions/Utilities.js
@@ -22,3 +22,11 @@ export function mapCardsToTypes(unsortedCards) {
 
     return results;
 }
+
+export function saveToLocalStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value))
+}
+
+export function loadFromLocalStorage(key) {
+    return JSON.parse(localStorage.getItem(key))
+}
