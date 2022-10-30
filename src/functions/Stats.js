886605904d892349ba85f6660e478ce3c5ea1333
noahvstephenson <nvstephenson@student.fullsail.edu>
2022-10-30 13:39:34 -0400

    Convert stats to Object
    - Changed stats data format to Objects + Keys from arrays
    - Allows 'color_counts.r' instead of  'color_counts[3]' to show how many red cards in a deck, for example

--- src/functions/Stats.js
+++ src/functions/Stats.js
@@ -24,41 +24,41 @@ export function getDeckStats(deck) {
             0, //8+
         ],
         //number of cards in a color
-        color_counts: [
-            0, //w
-            0, //u
-            0, //b
-            0, //r
-            0, //g
-            0, //c
-        ],
+        color_counts: {
+            w: 0,
+            u: 0,
+            b: 0,
+            r: 0,
+            g: 0,
+            c: 0,
+        },
         //percent of cards in a color
-        color_percents: [
-            0, //w
-            0, //u
-            0, //b
-            0, //r
-            0, //g
-            0, //c
-        ],
+        color_percents: {
+            w: 0,
+            u: 0,
+            b: 0,
+            r: 0,
+            g: 0,
+            c: 0,
+        },
         //number of mana production
-        mana_source_counts: [
-            0, //w
-            0, //u
-            0, //b
-            0, //r
-            0, //g
-            0, //c
-        ],
+        mana_source_counts: {
+            w: 0,
+            u: 0,
+            b: 0,
+            r: 0,
+            g: 0,
+            c: 0,
+        },
         //percent of mana production
-        mana_source_percents: [
-            0, //w
-            0, //u
-            0, //b
-            0, //r
-            0, //g
-            0, //c
-        ],
+        mana_source_percents: {
+            w: 0,
+            u: 0,
+            b: 0,
+            r: 0,
+            g: 0,
+            c: 0,
+        },
         //total price of deck
         total_prices: {
             usd: 0,
@@ -149,31 +149,25 @@ export function getDeckStats(deck) {
 
     //color_counts
 
-    let color_counts = [0,0,0,0,0,0]
+    let colors = ['w', 'u', 'b', 'r', 'g', 'c']
+    let color_counts = {
+        w: 0,
+        u: 0,
+        b: 0,
+        r: 0,
+        g: 0,
+        c: 0,
+    }
+
     for (let i = 0; i < dnl.length; i++){
         if (dnl[i].color_identity){
             if (dnl[i].color_identity == "[]") {
-                color_counts[5]++
+                color_counts['c']++
             }
             else {
-                for (let k = 0; k < dnl[i].color_identity.length; k++){
-                    switch (dnl[i].color_identity[k]) {
-                        case 'W':
-                            color_counts[0]++
-                            break
-                        case 'U':
-                            color_counts[1]++
-                            break
-                        case 'B':
-                            color_counts[2]++
-                            break
-                        case 'R':
-                            color_counts[3]++
-                            break
-                        case 'G':
-                            color_counts[4]++
-                            break
-                                                
+                for (let k = 0; k < colors.length; k++) {
+                    if (deck[i].color_identity.toLowerCase().includes(colors[k])) {
+                        color_counts[colors[k]]++
                     }
                 }
             }
@@ -187,41 +181,37 @@ export function getDeckStats(deck) {
 
     //color_percents
 
-    let color_percents = [0,0,0,0,0,0]
+    let color_percents = {
+        w: 0,
+        u: 0,
+        b: 0,
+        r: 0,
+        g: 0,
+        c: 0,
+    }
 
-    for (let i = 0; i < color_counts.length; i++){
-        color_percents[i] = Math.round(color_counts[i] / dnl.length * 100) / 100
+    for (let i = 0; i < colors.length; i++){
+        color_percents[colors[i]] = Math.round(color_counts[colors[i]] / dnl.length * 100) / 100
     }
     statResults.color_percents = color_percents
 
     //mana_source_counts
 
-    let mana_source_counts = [0,0,0,0,0,0]
+    let mana_source_counts = {
+        w: 0,
+        u: 0,
+        b: 0,
+        r: 0,
+        g: 0,
+        c: 0,
+    }
     let mana_source_counts_sum = 0
     for (let i = 0; i < deck.length; i++){
         if (deck[i].produced_mana){
             mana_source_counts_sum++
-            for (let k = 0; k < deck[i].produced_mana.length; k++){
-                switch (deck[i].produced_mana[k]) {
-                    case 'W':
-                        mana_source_counts[0]++
-                        break
-                    case 'U':
-                        mana_source_counts[1]++
-                        break
-                    case 'B':
-                        mana_source_counts[2]++
-                        break
-                    case 'R':
-                        mana_source_counts[3]++
-                        break
-                    case 'G':
-                        mana_source_counts[4]++
-                        break
-                    case 'C':
-                        mana_source_counts[5]++
-                        break
-                                            
+            for (let k = 0; k < colors.length; k++) {
+                if (deck[i].produced_mana.toLowerCase().includes(colors[k])) {
+                    mana_source_counts[colors[k]]++
                 }
             }
         }
@@ -234,11 +224,18 @@ export function getDeckStats(deck) {
 
     //mana_source_percents
 
-    let mana_source_percents = [0,0,0,0,0,0]
+    let mana_source_percents = {
+        w: 0,
+        u: 0,
+        b: 0,
+        r: 0,
+        g: 0,
+        c: 0,
+    }
 
     if (mana_source_counts_sum > 0) {
-        for (let i = 0; i < mana_source_counts.length; i++){
-            mana_source_percents[i] =  Math.round(mana_source_counts[i] / mana_source_counts_sum * 100) / 100
+        for (let i = 0; i < colors.length; i++){
+            mana_source_percents[colors[i]] =  Math.round(mana_source_counts[colors[i]] / mana_source_counts_sum * 100) / 100
         }
     }
 
