6fad166cca29e5944d9176d7d658c9d96f3f5cd5
noahvstephenson <nvstephenson@student.fullsail.edu>
2022-11-06 22:53:53 -0500

    Footer fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,13 +11,13 @@ const Footer = () => {
     return (
         
         <div className={`FooterContainer ${darkMode ? "FooterContainerDark" : ''}`}>
-            {/* <div className='NavigationBox'>
+            <div className='NavigationBox'>
                 <a className='Links' href='/'>Home</a><span> • </span>
                 <a className='Links' href='/login'>Login</a><span> • </span>
                 <a className='Links' href='/registration'>Register</a><span> • </span>
                 <a className='Links' href='/deckeditor'>New Deck</a><span> • </span>
                 <a className='Links' href='/cardcreator'>New Card</a>
-            </div> */}
+            </div>
             <div className={`Legality ${darkMode ? "LegalityDark" : ''}`}>
                 All the Cards is unofficial Fan Content permitted under the Fan Content Policy. Not approved/endorsed by Wizards.
                 Portions of the materials used are property of Wizards of the Coast. ©Wizards of the Coast LLC.
